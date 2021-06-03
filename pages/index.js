97072d946519fa6a04a56dec166b3615639b733b
maryam <mm.babrian@gmail.com>
2021-06-03 21:05:56 +0430

    1

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,7 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <a href="https://nextjs.org/docs" className={styles.card}></a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
