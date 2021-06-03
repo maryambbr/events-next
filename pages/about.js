874e07139e9af38703f4c41f8f99f570c93f3f0d
maryam <mm.babrian@gmail.com>
2021-06-03 21:55:37 +0430

    next

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+function About() {
+  return;
+  <div style={styles.container}>
+    <Link href="/home">go to home page</Link>
+  </div>;
+}
+
+export default About;
