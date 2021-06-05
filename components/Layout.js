837ecc5b4c8c466329cdd42db6c1fd296ffe05fe
maryam <mm.babrian@gmail.com>
2021-06-06 00:58:42 +0430

    layouts and some basic styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,12 @@
 import Head from 'next/head';
-import styles from '../styles/Layout.module.css';
+import { useRouter } from 'next/router';
+import Header from './Header';
+import Footer from './Footer';
+import ShowCase from './ShowCase';
+import styles from '@/styles/Layout.module.css';
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
   return (
     <div>
       <Head>
@@ -9,7 +14,10 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
       </Head>
+      <Header />
+      {router.pathname === '/' && <ShowCase />}
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
