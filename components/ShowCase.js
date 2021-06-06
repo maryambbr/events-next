cc9d0c73d8ab7fcb1a0660fe6ba2ab6267461e66
maryam <mm.babrian@gmail.com>
2021-06-06 13:09:37 +0430

    some changes

--- components/ShowCase.js
+++ components/ShowCase.js
@@ -3,7 +3,7 @@ import styles from '@/styles/ShowCase.module.css';
 export default function ShowCase() {
   return (
     <div className={styles.showcase}>
-      <h1>Welcome To This Journey</h1>
+      <h1>Welcome To This Journey Find Your Events</h1>
       <h2>Find Your Interesting Events Here</h2>
     </div>
   );
